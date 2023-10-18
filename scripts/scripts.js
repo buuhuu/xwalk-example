7861b6efbbe3750a6340b2b5ef30a1f566d56ce3
Dirk <drudolph@adobe.com>
2023-10-18 10:41:59 +0200

    fix: remove hero auto blocking

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,21 +16,6 @@ import {
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 /**
- * Builds hero block and prepends to main in a new section.
- * @param {Element} main The container element
- */
-function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
-  }
-}
-
-/**
  * load fonts.css and set a session storage flag
  */
 async function loadFonts() {
@@ -48,7 +33,7 @@ async function loadFonts() {
  */
 function buildAutoBlocks(main) {
   try {
-    buildHeroBlock(main);
+    return;
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
