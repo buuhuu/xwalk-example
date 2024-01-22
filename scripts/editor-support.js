393c34d49542fad42cd5bf98265778b27409d7b8
Duy Nguyen <duy.phnguyen@gmail.com>
2024-01-22 19:34:21 +0100

    feat: update to latest ue data attributes

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -1,32 +1,37 @@
-import {
-  decorateBlock, decorateButtons, decorateIcons, loadBlock,
-} from './lib-franklin.js';
+import { decorateBlock, decorateButtons, decorateIcons, loadBlock } from './lib-franklin.js';
+
+const connectionPrefix = 'urn:aemconnection:';
 
 function handleEditorUpdate(event) {
-  const { detail: { responseData } } = event;
-  const updates = responseData?.updates;
+  const { detail } = event;
+
+  const resource = detail?.requestData?.target?.resource;
+  if (!resource) return;
+
+  const element = document.querySelector(`[data-aue-resource="${resource}"]`);
+  const block = element?.closest('.block');
+  const blockResource = block?.getAttribute('data-aue-resource');
+  if (!block || !blockResource?.startsWith(connectionPrefix)) return;
+
+  const updates = detail?.responseData?.updates;
   Promise.all(updates
     .map(async (update) => {
-      const { resource, content } = update;
-      const block = document.querySelector(`[data-aue-resource="${resource}"]`);
-      const blockItemId = block?.getAttribute('data-aue-resource');
-      if (block && blockItemId?.startsWith('urn:aemconnection:')) {
-        const newBlockDocument = new DOMParser().parseFromString(content, 'text/html');
-        const newBlock = newBlockDocument?.querySelector(`[data-aue-resource="${blockItemId}"]`);
-        if(newBlock) {
-          newBlock.style.display = 'none';
-          block.insertAdjacentElement('afterend', newBlock);
-          // decorate buttons and icons
-          decorateButtons(newBlock);
-          decorateIcons(newBlock);
-          // decorate and load the block
-          decorateBlock(newBlock);
-          await loadBlock(newBlock);
-          // remove the old block and show the new one
-          block.remove();
-          newBlock.style.display = null;
-          return Promise.resolve();
-        }
+      const { content } = update;
+      const newBlockDocument = new DOMParser().parseFromString(content, 'text/html');
+      const newBlock = newBlockDocument?.querySelector(`[data-aue-resource="${blockResource}"]`);
+      if(newBlock) {
+        newBlock.style.display = 'none';
+        block.insertAdjacentElement('afterend', newBlock);
+        // decorate buttons and icons
+        decorateButtons(newBlock);
+        decorateIcons(newBlock);
+        // decorate and load the block
+        decorateBlock(newBlock);
+        await loadBlock(newBlock);
+        // remove the old block and show the new one
+        block.remove();
+        newBlock.style.display = null;
+        return Promise.resolve();
       }
       return Promise.reject();
     }))
